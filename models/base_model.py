ef4b2d00bee91884d907375fb815598dd731f68f
Hossien <hkargar17@gmail.com>
2024-10-10 01:36:01 +0330

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,17 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql+psycopg2://postgres:1234@localhost/restaurant_db'
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
